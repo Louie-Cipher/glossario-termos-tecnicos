694ddf40c6c78b58a9ed6856277f7489826f3689
Louie-Cipher <louie.spena@outlook.com>
2022-10-10 11:56:55 -0300

    dynamic end message

--- src/Components/GameOver/index.tsx
+++ src/Components/GameOver/index.tsx
@@ -1,5 +1,14 @@
 import * as Comp from './styles';
 
+const Phrase = (percentage: number) => {
+    if (percentage === 100) return '🎉 Parabéns, acertou tudo! 🎉';
+    if (percentage >= 80) return '🎉 Muito bom! 🎉';
+    if (percentage >= 50) return 'Bom!';
+    if (percentage >= 30) return 'Pode melhorar...';
+    if (percentage > 0) return 'Ruim demais';
+    return 'Errou tudo... Ruim demais kk';
+};
+
 interface Props {
     points: number;
     questionsCount: number;
@@ -13,11 +22,11 @@ export default ({ points, questionsCount, startGame }: Props) => {
                 <Comp.GameOverTitle>Fim de jogo!</Comp.GameOverTitle>
                 <Comp.GameOverMessage>
                     Você acertou {points} de {questionsCount} termos
+                    <br />
+                    {Phrase(Math.round((points / questionsCount) * 100))}
                 </Comp.GameOverMessage>
 
-                <Comp.PlayAgainButton onClick={startGame}>
-                    Jogar novamente
-                </Comp.PlayAgainButton>
+                <Comp.PlayAgainButton onClick={startGame}>Jogar novamente</Comp.PlayAgainButton>
             </Comp.GameOver>
         </>
     );
