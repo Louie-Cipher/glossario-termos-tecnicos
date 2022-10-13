9c3add98ca9fe760d5a8f78cd06307a2ba021476
Louie-Cipher <louie.spena@outlook.com>
2022-10-13 16:22:23 -0300

    gameOver emojis

--- src/Components/GameOver/index.tsx
+++ src/Components/GameOver/index.tsx
@@ -3,10 +3,10 @@ import * as Comp from './styles';
 const Phrase = (percentage: number) => {
     if (percentage === 100) return '🎉 Parabéns, acertou tudo! 🎉';
     if (percentage >= 80) return '🎉 Muito bom! 🎉';
-    if (percentage >= 50) return 'Bom!';
-    if (percentage >= 30) return 'Pode melhorar...';
-    if (percentage > 0) return 'Ruim demais';
-    return 'Errou tudo... Ruim demais kk';
+    if (percentage >= 50) return 'Bom! :)';
+    if (percentage >= 30) return '🤔 Pode melhorar...';
+    if (percentage > 0) return 'Muito ruim 😢';
+    return 'Errou tudo... Ruim demais kk 🤣';
 };
 
 interface Props {
