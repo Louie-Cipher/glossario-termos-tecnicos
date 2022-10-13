f848d9ce506252f07a329a6bc06aade664c318f2
Louie-Cipher <louie.spena@outlook.com>
2022-10-13 16:21:38 -0300

    styling changes

--- src/styles.ts
+++ src/styles.ts
@@ -17,10 +17,12 @@ export const Content = styled.div`
     display: flex;
     flex: 1;
     flex-direction: column;
+    min-width: 400px;
+    max-width: 600px;
     text-align: center;
-    width: 80%;
     background-color: #333;
     padding: 0 20px 20px;
+    margin: 10px 0;
     border-radius: 10px;
     color: #fff;
     box-shadow: 0 0 10px #000;
@@ -65,13 +67,16 @@ export const Info = styled.div`
     align-items: center;
     justify-content: space-between;
     width: 100%;
+    margin: 0;
+    padding: 0;
 `;
 
 export const Points = styled.p`
     font-size: 1em;
     font-weight: bold;
-    margin: 20px 0;
     text-align: right;
+    margin: 0;
+    padding: 0;
 `;
 
 export const CurrentRound = styled.span`
@@ -97,24 +102,32 @@ export const Question = styled.div`
 `;
 
 export const QuestionTitle = styled.p`
+    max-width: 70%;
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+    justify-content: center;
     font-size: 1.6rem;
     font-weight: 500;
+    flex-wrap: wrap;
 `;
 
 export const QuestionTitleBold = styled.span`
     font-size: 1.6rem;
     font-weight: 900;
+    margin-left: 5px;
 `;
 
 export const QuestionAnswers = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    width: 100%;
     margin-top: 2rem;
 `;
 
 export const QuestionAnswer = styled.button`
-    width: 100%;
+    width: 90%;
     padding: 1rem;
     margin-top: 1rem;
     border: none;
@@ -122,11 +135,15 @@ export const QuestionAnswer = styled.button`
     box-shadow: 0 0 10px #000a;
     color: #fff;
     font-size: 1rem;
-    font-weight: 600; 
+    font-weight: 600;
     cursor: pointer;
     transition: all 0.1s;
 
     :hover {
+        padding: 1.5rem 1rem;
+        transition: all 0.1s;
+    }
+    :active {
         padding: 1.5rem 1rem;
         transition: all 0.1s;
     }
